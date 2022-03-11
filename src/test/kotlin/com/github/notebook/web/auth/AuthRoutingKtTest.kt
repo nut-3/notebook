8bb7c65b33adb1afa7ab511dad7acba55cad3fdf
nut-3 <kshurukhin@gmail.com>
2022-03-11 18:41:32 +0300

    implement AuthRoutingKtTest.kt
    Added test for authentication.

---
+++ src/test/kotlin/com/github/notebook/web/auth/AuthRoutingKtTest.kt
@@ -0,0 +1,29 @@
+package com.github.notebook.web.auth
+
+import com.github.notebook.ServerTest
+import com.github.notebook.USER_PASSWORD
+import com.github.notebook.user
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class AuthRoutingKtTest : ServerTest() {
+
+    @Test
+    fun `Test Login page`() = runTest {
+
+        client.submitForm("${AuthRouting().root}/login") {
+            setBody(
+                FormDataContent(
+                    parametersOf("username" to listOf(user.name), "password" to listOf(USER_PASSWORD))
+                )
+            )
+        }.apply {
+            Assertions.assertThat(this.status).isEqualTo(HttpStatusCode.OK)
+            Assertions.assertThat(this.bodyAsText()).contains("Hello, ${user.name}!")
+        }
+    }
+}
