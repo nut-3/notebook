28ace036971fcb726c745f39a8d8828dbd1d0543
nut-3 <kshurukhin@gmail.com>
2022-03-13 15:42:50 +0300

    fix parameter naming

--- src/main/kotlin/com/github/notebook/service/NotebookService.kt
+++ src/main/kotlin/com/github/notebook/service/NotebookService.kt
@@ -16,16 +16,16 @@ object NotebookService {
         Notebooks.select { Notebooks.userId eq userId }.mapNotNull { Notebooks.toNotebook(it) }
     }
 
-    fun getByIdAndUserId(id: Int, userId: Int) = transaction {
+    fun getByIdAndUserId(notebookId: Int, userId: Int) = transaction {
         addLogger(Slf4jSqlDebugLogger)
-        Notebooks.select { Notebooks.userId eq userId and (Notebooks.id eq id) }.map { Notebooks.toNotebook(it) }
-            .singleOrNull() ?: throw NotFoundException("Notebook with id=$id and userId=$userId not found")
+        Notebooks.select { Notebooks.userId eq userId and (Notebooks.id eq notebookId) }.map { Notebooks.toNotebook(it) }
+            .singleOrNull() ?: throw NotFoundException("Notebook with id=$notebookId and userId=$userId not found")
     }
 
-    fun updateByIdAndUserId(notebook: EditNotebook, id: Int, userId: Int) = transaction {
+    fun updateByIdAndUserId(notebook: EditNotebook, notebookId: Int, userId: Int) = transaction {
         addLogger(Slf4jSqlDebugLogger)
-        if (notebook.id != id) throw IllegalArgumentException("Wrong notebook data")
-        val updatedRows = Notebooks.update({Notebooks.id eq id}) {
+        if (notebook.id != notebookId) throw IllegalArgumentException("Wrong notebook data")
+        val updatedRows = Notebooks.update({Notebooks.id eq notebookId}) {
             it[name] = notebook.name
             it[updated] = LocalDateTime.now()
         }
@@ -41,15 +41,15 @@ object NotebookService {
         return@transaction get(newId.value)
     }
 
-    fun deleteByIdAndUserId(id: Int, userId: Int) = transaction {
+    fun deleteByIdAndUserId(notebookId: Int, userId: Int) = transaction {
         addLogger(Slf4jSqlDebugLogger)
-        val updatedRows = Notebooks.deleteWhere { Notebooks.id eq id }
-        if (updatedRows == 0) throw NotFoundException("Notebook with id=$id and userId=$userId not found")
+        val updatedRows = Notebooks.deleteWhere { Notebooks.id eq notebookId }
+        if (updatedRows == 0) throw NotFoundException("Notebook with id=$notebookId and userId=$userId not found")
     }
 
     fun get(notebookId: Int) = transaction {
         addLogger(Slf4jSqlDebugLogger)
         Notebooks.select { Notebooks.id eq notebookId }.map { Notebooks.toNotebook(it) }
-            .singleOrNull() ?: throw NotFoundException("Notebook with id=$id not found")
+            .singleOrNull() ?: throw NotFoundException("Notebook with id=$notebookId not found")
     }
 }
